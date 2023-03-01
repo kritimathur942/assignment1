c75475fea462b36830afbf05bf767ebaa0d8a3de
shailja168 <shailjatripathi168@gmail.com>
2023-03-01 13:03:36 +0530

    modulo added

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from subtraction import subtract
 from multiplication import multiply
 from division import divide
 from power import power
+from modulo import modulo
 
 # User input
-print("Select operation.\n1.Add\n2.Subtract\n3.Multiply\n4.Divide\n5.Power")
+print("Select operation.\n1.Add\n2.Subtract\n3.Multiply\n4.Divide\n5.Power\n6.Modulo")
 
 # Take input from the user
-choice = input("Enter choice (1/2/3/4/5): ")
+choice = input("Enter choice (1/2/3/4/5/6): ")
 
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
@@ -27,5 +28,9 @@ elif choice == '4':
 
 elif choice == '5':
    print(num1,"^",num2,"=", power(num1,num2))
+
+elif choice == '6':
+   print(num1,"%",num2,"=", modulo(num1,num2))
+
 else:
    print("Invalid input")
---
+++ modulo.py
@@ -0,0 +1,3 @@
+# Function to find modulo of two numbers
+def modulo(x, y):
+   return x % y
